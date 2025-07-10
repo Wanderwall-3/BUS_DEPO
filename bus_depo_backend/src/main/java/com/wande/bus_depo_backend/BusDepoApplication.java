54024a394d829b778187f3643bd260b9f64a95d4
Vignesh Sathya <118740959+Wanderwall-3@users.noreply.github.com>
2025-07-10 12:32:52 +0530

    Update BusDepoApplication.java

--- bus_depo_backend/src/main/java/com/wande/bus_depo_backend/BusDepoApplication.java
+++ bus_depo_backend/src/main/java/com/wande/bus_depo_backend/BusDepoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EntityScan("com.wande.bus_depo_backend.model")
 public class BusDepoApplication {
 
 	public static void main(String[] args) {
