c75e63c372d219201558ef1c3132998200807868
Vignesh Sathya <118740959+Wanderwall-3@users.noreply.github.com>
2025-07-10 12:38:37 +0530

    Update BusDepoApplication.java

--- bus_depo_backend/src/main/java/com/wande/bus_depo_backend/BusDepoApplication.java
+++ bus_depo_backend/src/main/java/com/wande/bus_depo_backend/BusDepoApplication.java
@@ -2,6 +2,8 @@ package com.wande.bus_depo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.wande.bus_depo_backend.model")
